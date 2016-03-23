98a74c31143cf7853664113929961498a175fa48
Danny Bauman <dannybauman@gmail.com>
2016-03-23 11:56:12 -0700

    keep a running score for every publisher on how their connection seems to be doing

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ module.exports = WebSocketMirror
 var ws = require('ws')
 var inherits = require('inherits')
 
+var CONNECTION_CHECK_INTERVAL = 5000 // check the publisher's connection and generate a score every 5 secs
+var MIN_REQUIRED_TIMESTAMP_COUNT = CONNECTION_CHECK_INTERVAL / 100
+
 inherits(WebSocketMirror, ws.Server)
 
 function WebSocketMirror (httpServer) {
@@ -12,6 +15,7 @@ function WebSocketMirror (httpServer) {
 
   ws.Server.call(this, { server: httpServer })
   this.channels = {}
+  this._publishers = {}
   this.on('connection', this._onconnection.bind(this))
 }
 
@@ -24,6 +28,11 @@ WebSocketMirror.prototype._onconnection = function (socket) {
   var channel = path[0]
   var action = path[1]
   if (action === 'publish') {
+    this._publishers[channel] = socket
+    this._publishers[channel]._timestamps = []
+    this._publishers[channel]._connectionScore = 5
+    this._publishers[channel]._timer = setTimeout(this._checkconnection.bind(this, channel), CONNECTION_CHECK_INTERVAL)
+    socket.on('close', this._onpublisherclose.bind(this, channel, socket))
     socket.on('message', this._onmessage.bind(this, channel))
   } else if (action === 'subscribe') {
     this.channels[channel] = this.channels[channel] || []
@@ -34,6 +43,9 @@ WebSocketMirror.prototype._onconnection = function (socket) {
 }
 
 WebSocketMirror.prototype._onmessage = function (channel, message) {
+  // this may get called by subscribers before there is a publisher,
+  // so only add to the publisher's timestamps if the publisher has connected
+  this._publishers[channel] && this._publishers[channel]._timestamps.push(Date.now())
   var subscribers = this.channels[channel]
   for (var i in subscribers) {
     if (subscribers[i]._paused) continue
@@ -55,6 +67,58 @@ WebSocketMirror.prototype._oncontrolmessage = function (channel, socket, control
   }
 }
 
+WebSocketMirror.prototype._checkconnection = function (channel) {
+  if (!this._publishers[channel]) return
+
+  var previousTimestamps = this._publishers[channel]._previousTimestamps
+  var currentTimestamps = this._publishers[channel]._timestamps
+  if (previousTimestamps && currentTimestamps) {
+    var diff = previousTimestamps.length - currentTimestamps.length
+    var connectionScore = this._publishers[channel]._connectionScore
+
+    // if the publisher hasn't met the minimum, cap their score at 5
+    if (currentTimestamps.length < MIN_REQUIRED_TIMESTAMP_COUNT && connectionScore > 5) {
+      console.log('minimum not met, only got: ' + currentTimestamps.length)
+      connectionScore = 5
+    }
+
+    if (currentTimestamps.length === 0) {
+      console.log('got none since last check')
+    } else if (diff > 10) {
+      connectionScore -= 5
+    } else if (diff > 5) {
+      connectionScore -= 3
+    } else if (diff > 2) {
+      connectionScore -= 2
+    } else if (diff === 2) {
+      connectionScore -= 1
+    } else if (diff === 1) {
+      // since the diff was so small, consider it no change
+      connectionScore = connectionScore
+    } else {
+      connectionScore += 1
+    }
+
+    if (connectionScore > 10) {
+      connectionScore = 10
+    } else if (connectionScore < 0) {
+      connectionScore = 0
+    }
+
+    //if (diff > 0) {
+      console.log('previous: ' + previousTimestamps.length + ', current: ' + currentTimestamps.length)
+      console.log(connectionScore)
+    //}
+    this._publishers[channel]._connectionScore = connectionScore
+  }
+
+  this._publishers[channel]._previousTimestamps = currentTimestamps
+  this._publishers[channel]._timestamps = []
+
+  clearTimeout(this._publishers[channel]._timer)
+  this._publishers[channel]._timer = setTimeout(this._checkconnection.bind(this, channel), CONNECTION_CHECK_INTERVAL)
+}
+
 WebSocketMirror.prototype._onsubscriberclose = function (channel, socket) {
   this.channels[channel] = this.channels[channel].filter(function (s) {
     return s !== socket
@@ -63,4 +127,10 @@ WebSocketMirror.prototype._onsubscriberclose = function (channel, socket) {
   if (Object.keys(this.channels[channel]).length === 0) {
     delete this.channels[channel]
   }
+}
+
+WebSocketMirror.prototype._onpublisherclose = function (channel, socket) {
+  clearTimeout(this._publishers[channel]._timer)
+  delete this._publishers[channel]
+  console.log('publisher for channel ' + channel + ' closed')
 }
